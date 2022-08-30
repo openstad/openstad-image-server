42428e7dd2e5cedfd79eea3526f7b354b45cc2b6
Rudi van Hierden <rudi@draad.nl>
2022-08-30 13:53:31 +0200

    Add `MYSQL_CA_CERT` for MySQL SSL connection

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,25 @@
 // Update with your config settings.
 require('dotenv').config();
 
+const connection = {
+  host:     process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user:     process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+};
+
+if (process.env.MYSQL_CA_CERT) {
+	connection.ssl = {
+		rejectUnauthorized: true,
+		ca: [ process.env.MYSQL_CA_CERT ]
+	}
+}
+
 module.exports = {
 
   development: {
     client: 'mysql',
-    connection: {
-      host:     process.env.DB_HOST || 'localhost',
-      database: process.env.DB_NAME,
-      user:     process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      port: process.env.DB_PORT || '3306',
-    },
+    connection,
     migrations: {
       directory: __dirname + '/knex/migrations',
     },
@@ -22,13 +30,7 @@ module.exports = {
 
   production: {
     client: 'mysql',
-    connection: {
-      host:     process.env.DB_HOST || 'localhost',
-      database: process.env.DB_NAME,
-      user:     process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      port: process.env.DB_PORT || '3306',
-    },
+    connection,
     migrations: {
       directory: __dirname + '/knex/migrations',
     },
